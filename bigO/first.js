c1266f16ebb5020390507fc40abf7ff4172ae0eb
vd89 <dixitvora@gmail.com>
2020-12-05 09:54:59 +0530

    main: calculation of space complexity

--- bigO/first.js
+++ bigO/first.js
@@ -92,3 +92,21 @@ function printAllNumbersThenAllPairSums(numbers) {
 }
 
 printAllNumbersThenAllPairSums([1, 2, 3, 4, 5]); // O(n^2)
+
+//#5 Space complexity O(1)
+function boooo(n) {
+  for (let i = 0; i < n; i++) {
+    console.log('booooo');
+  }
+}
+
+// #6 Space complexity O(n)
+function arrayOfHiNTimes(n) {
+  var hiArray = [];
+  for (let i = 0; i < n; i++) {
+    hiArray[i] = 'hi';
+  }
+  return hiArray;
+}
+
+arrayOfHiNTimes(6);
